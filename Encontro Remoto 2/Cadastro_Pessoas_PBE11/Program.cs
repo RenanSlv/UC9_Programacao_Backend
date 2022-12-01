00325d50abef4de821b493f9ed23838f738c4d16
RenanSlv <renan.romulo1337@gmail.com>
2022-12-01 10:07:32 -0300

    Cadastro_Pessoa_UC9

---
+++ Encontro Remoto 2/Cadastro_Pessoas_PBE11/Program.cs
@@ -0,0 +1,22 @@
+﻿//instanciar um objeto da classe PessoaFisica
+using Cadastro_Pessoas_PBE11.Classes;
+
+//Instanciar um objeto da classe PessoaFisica
+PessoaFisica novaPf = new PessoaFisica();
+
+//atribuindo valores papra os atributos do objeto
+novaPf.Nome = "Renan";
+novaPf.Rendimento = 50000.58f;
+
+//imprimindo no console os valores desses atributos
+Console.WriteLine(novaPf.Nome);
+Console.WriteLine(novaPf.Rendimento);
+
+//Instanciar um objeto da classe PessoaJuridica
+PessoasJuridica novaPj = new PessoasJuridica();
+//atribuimos valores para os atributos do objeto
+novaPj.RazaoSocial = "Senai Informática";
+
+//Imprimindo no console os valores desses atributos
+Console.WriteLine("Razão Social Pj : " + novaPj.RazaoSocial );
+Console.WriteLine($"Razão Social Pj : {novaPj.RazaoSocial} ");
