ad09f9de82b5d0f24e48262a560c2210886b1cc0
RenanSlv <renan.romulo1337@gmail.com>
2023-02-01 08:24:36 -0300

    Conteudo desenvolvido no encontro remoto 7

---
+++ Encontro Remoto 7/Cadastro_Pessoas_PBE11/Interfaces/IPessoa.cs
@@ -0,0 +1,8 @@
+namespace Cadastro_Pessoas_PBE11.Interfaces
+{
+    public interface IPessoa
+    {
+        //tip nome(tipoArgumento nomeArgumento)
+        float PagarImposto(float rendimento);
+    }
+}
